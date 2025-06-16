3bbec386890ffaeeaacbcc09816694cae8bc0790
Parves <mohammadparvesbd02@gmail.com>
2025-06-16 11:02:34 +0300

    m

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,17 +1,4 @@
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
-  fileFilter: function (req, file, cb) {
-    const fileTypes = /jpeg|jpg|png|pdf|png/;
-    const extname = fileTypes.test(file.originalname.toLowerCase());
-    const mimetype = fileTypes.test(file.mimetype);
+import multer from "multer"
 
-    if (mimetype && extname) {
-      return cb(null, true);
-    } else {
-      cb(new Error('Only images and PDFs are allowed!'));
-    }
-  }
-});
-
-export default upload;
+const storage = multer.memoryStorage();
+export const singleUpload = multer({storage}).single("file");
