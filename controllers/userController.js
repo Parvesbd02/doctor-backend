e87f9a51baa89892fe42885cdbc0799b72a7bd11
Parves <mohammadparvesbd02@gmail.com>
2025-06-16 10:19:12 +0300

    x

--- controllers/userController.js
+++ controllers/userController.js
@@ -315,15 +315,6 @@ const cancelAppointment = async (req, res) => {
   }
 };
 
-const razorpayInstant = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET
-})
-
-//API FOR ONLINE PAYMENT 
-const paymentRazorpay = async(req,res) =>{
-  
-}
 
 
 
