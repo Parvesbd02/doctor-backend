9a007140913e2577809efb5244a67445dbfb4f67
Parves <mohammadparvesbd02@gmail.com>
2025-06-16 10:52:56 +0300

    multer

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,47 +1,17 @@
-// backend/middleware/multer.js
-
-import multer from "multer";
-import path from "path";
-import fs from "fs";
-
-// Define storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const uploadPath = "uploads/";
-
-    if (!fs.existsSync(uploadPath)) {
-      fs.mkdirSync(uploadPath, { recursive: true });
-    }
-    cb(null, uploadPath);
-  },
-  filename: function (req, file, cb) {
-    const ext = path.extname(file.originalname);
-    const fileName = Date.now() + ext;
-    cb(null, fileName);
-  },
-});
-
-// Create multer upload instance
 const upload = multer({
   storage: storage,
-  limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB max file size
-  },
-  fileFilter: (req, file, cb) => {
-    if (!file) {
-      cb(null, true); // Allow no file selected
-    } else {
-      const filetypes = /jpeg|jpg|png/;
-      const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-      const mimetype = filetypes.test(file.mimetype);
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  fileFilter: function (req, file, cb) {
+    const fileTypes = /jpeg|jpg|png|pdf|png/;
+    const extname = fileTypes.test(file.originalname.toLowerCase());
+    const mimetype = fileTypes.test(file.mimetype);
 
-      if (extname && mimetype) {
-        cb(null, true);
-      } else {
-        cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
-      }
+    if (mimetype && extname) {
+      return cb(null, true);
+    } else {
+      cb(new Error('Only images and PDFs are allowed!'));
     }
-  },
+  }
 });
 
 export default upload;
